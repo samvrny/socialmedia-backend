4e7c3bbfcaecb66621bc15dd9e4cd8ef208fb885
samvrny <samvrny@gmail.com>
2022-09-21 14:44:48 -0500

    set up the user model

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-Social Media Back-End
+# Social Media Back-End
 
 ## Description
 This project is a back end API for a hypothetical social media site build using MongoDB, Mongoose, and Express
---
+++ models/User.js
@@ -0,0 +1,44 @@
+const { Schema, model } = require('mongoose');
+
+const UserSchema = new Schema(
+    {
+        username: {
+            type: String, 
+            unique: true,
+            required: true,
+            trim: true
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            match: [/^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/, 'Please fill out a valid email address']
+        },
+        thoughts: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'Thought'
+            }
+        ],
+        friends: [ //may need to change later, we will see.
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'User'
+            }
+        ]
+    },
+    {
+        toJSON: {
+            virtuals: true
+        },
+        id: false //not sure why I put this here it's in the pizza model though
+    }
+);
+
+UserSchema.virtual('friendCount').get(function() {
+    return this.friends.reduce((total, friend) => total + friend.length + 1, 0);
+})
+
+const User = model('User', UserSchema);
+
+module.exports = User;
